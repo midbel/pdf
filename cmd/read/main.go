034c9af61b1d2a214791bcc83071a83a3012ef38
midbel <brun.nicolas21@gmail.com>
2021-02-26 10:07:29 +0100

    select page range

--- cmd/read/main.go
+++ cmd/read/main.go
@@ -10,6 +10,54 @@ import (
 	"github.com/midbel/pdf"
 )
 
+type Ranger interface {
+	Pages() []int
+}
+
+type Single struct {
+	page int
+}
+
+func makeSingle(str string) (Ranger, error) {
+	n, err := strconv.Atoi(str)
+	if err != nil {
+		return nil, err
+	}
+	return Single{page: n}, nil
+}
+
+func (s Single) Pages() []int {
+	return []int{s.page}
+}
+
+type Interval struct {
+	first int
+	last  int
+}
+
+func makeInterval(from, to string) (Ranger, error) {
+	fst, err := strconv.Atoi(from)
+	if err != nil {
+		return nil, err
+	}
+	lst, err := strconv.Atoi(to)
+	if err != nil {
+		return nil, err
+	}
+	if fst >= last {
+		return nil, fmt.Errorf("invalid interval (%d - %d)", fst, lst)
+	}
+	return Interval{first: fst, last: lst}, nil
+}
+
+func (i Interval) Pages() []int {
+	var ps []int
+	for j := i.first; j <= i.last; j++ {
+		ps = append(ps, j)
+	}
+	return ps
+}
+
 // a range is defined with
 // : = all pages
 // x: = from page X to end of document
@@ -18,30 +66,66 @@ import (
 // x,y,z = list of page
 // possible to mix range and individual page
 type Range struct {
-	page int
+	pages []Ranger
 }
 
-func (r *Range) Set(str string) (err error) {
-	if str == ":" {
-		return nil
+func (r *Range) Set(str string) error {
+	var i int
+	for j, b := range str {
+		switch b {
+		case ',':
+			g, err := makeSingle(str[i:j])
+			if err != nil {
+				return err
+			}
+			i = j + 1
+			r.pages = append(r.pages, g)
+		case ':':
+			k := j+1
+			for ; k < len(str); k++ {
+				if str[k] == ',' {
+					break
+				}
+				if str[k] == ':' {
+					return fmt.Errorf("syntax error: unexpected colon")
+				}
+			}
+			g, err := makeInterval(str[i:j], str[j+1:k])
+			if err != nil {
+				return err
+			}
+			r.pages = append(r.pages, g)
+			i = k+1
+		default:
+		}
 	}
-	r.page, err = strconv.Atoi(str)
-	return err
+	if i > 0 && i < len(str) {
+		g, err := makeSingle(str[i:])
+		if err != nil {
+			return err
+		}
+		r.pages = append(r.pages, g)
+	}
+	return nil
 }
 
 func (r *Range) String() string {
-	if r.page == 0 {
+	if len(r.pages) == 0 {
 		return "outline"
 	}
-	return fmt.Sprintf("page #%d", r.page)
+	return "page"
 }
 
 func (r *Range) Pages(n int64) []int {
-	return []int{r.page}
+	var ps []int
+	for _, p := range r.pages {
+		ps = append(ps, p.Pages()...)
+	}
+	return ps
 }
 
 func (r *Range) IsEmpty() bool {
-	return r.page == 0
+	return len(r.pages) == 0
 }
 
 func main() {
